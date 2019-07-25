a93f7a47de0110087c40f9edc72d55fceacc7b9d
fu11010 <1694190761@qq.com>
2019-07-25 15:41:36 +0800

    提交了 Emp s实体类

---
+++ src/main/java/com/example/demo/entity/Emp.java
@@ -0,0 +1,21 @@
+package com.example.demo.entity;
+
+/**
+ * @description: Emp
+ * @author: ${༺评༒生༻}
+ * @date: 2019-07-25 15:38
+ * @Version 1.0
+ */
+public class Emp {
+    private  Integer deptno;
+    private  String dname;
+
+
+    @Override
+    public String toString() {
+        return "Dept{" +
+                "deptno=" + deptno +
+                ", dname='" + dname + '\'' +
+                '}';
+    }
+}
